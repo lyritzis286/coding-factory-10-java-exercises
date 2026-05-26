19db43cc05944fdffb8ac4cee7521b329a96a261
lyritzis286 <lyritzis286@gmail.com>
2026-05-26 18:52:39 +0300

    Add OnlyOneCodingFactory and OnlyOneCodingFactoryLazy classes with singleton implementation; update Main to demonstrate usage

---
+++ src/gr/aueb/cf/ch13/OnlyOneCodingFactoryLazy.java
@@ -0,0 +1,21 @@
+package gr.aueb.cf.ch13;
+
+public class OnlyOneCodingFactoryLazy {
+
+    private static OnlyOneCodingFactoryLazy instance; //null
+
+    private OnlyOneCodingFactoryLazy() {
+
+    }
+
+    public static OnlyOneCodingFactoryLazy getInstance() {
+        if (instance == null) {
+            instance = new OnlyOneCodingFactoryLazy();
+        }
+        return instance;
+    }
+
+    public void doSomething() {
+        System.out.println("Say Hello");
+    }
+}
