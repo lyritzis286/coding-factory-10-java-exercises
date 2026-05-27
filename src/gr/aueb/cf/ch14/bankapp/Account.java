b34eba1afe6586a713dc4165e536d99d8289b803
lyritzis286 <lyritzis286@gmail.com>
2026-05-27 19:05:13 +0300

    Add Account class with deposit and withdraw methods; implement custom exceptions for error handling

---
+++ src/gr/aueb/cf/ch14/bankapp/Account.java
@@ -0,0 +1,139 @@
+package gr.aueb.cf.ch14.bankapp;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Account {
+    private long id;
+    private String accountNumber;
+    private String firstname;
+    private String lastname;
+    private String ssn;
+    private double balance;
+
+    public Account() {
+
+    }
+
+    public Account(long id, String accountNumber, String firstname, String lastname, String ssn, double balance) {
+        this.id = id;
+        this.accountNumber = accountNumber;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.ssn = ssn;
+        this.balance = balance;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    //Public API - Contract
+
+    /**
+     * Deposit a specific amount of money into the account.
+     *
+     * @param amount    the amount of money to be deposited, must be positive.
+     * @throws NegativeAmountException if the amount is negative.e.
+     */
+    public void deposit(double amount) throws NegativeAmountException{
+        try {
+           if (amount < 0) {
+               throw new NegativeAmountException("Amount must be positive");
+           }
+           balance += amount;
+           //audit trail: who, when, what, initial balance, resulting balance
+        }
+        catch (NegativeAmountException e) {
+            System.err.printf("Negative amount = %f is not allowed. \n%s\n",amount, e.getMessage());
+            throw e;
+        }
+    }
+
+    /**
+     * Withdraw a specific amount of money from the account.
+     * @param amount    the amount of money to be withdrawn, must be positive and less than or equal to the current balance.
+     * @param ssn       the SSN of the account holder.
+     * @throws Exception if the amount is negative, the SSN does not match, or there are insufficient funds.
+     */
+    public void withdraw(double amount, String ssn)throws Exception {
+        try {
+            if (amount < 0) {
+                throw new NegativeAmountException("The amount = " + amount + " must not be negative");
+            }
+            if (!isSsnValid(ssn)) {
+                throw new SsnNotValidException("SSN does not match account holder's SSN");
+            }
+            if (balance < amount) {
+                throw new InsufficientBalanceException("The balance = " + balance + " is not sufficient");
+            }
+            balance -= amount;
+            //audit trail :who, when, what, initial balance , resulting balance
+        }
+        catch (NegativeAmountException | SsnNotValidException | InsufficientBalanceException e) {
+            System.err.printf(LocalDateTime.now() + ": Withdrawal  failed. \n%s\n", e.getMessage());     //Logging
+            throw e;
+        }
+
+
+    }
+
+    //Design - Delegation
+
+    /**
+     * Get the account balance.
+     * @return  the account balance.
+     */
+    public double getAccountBalance() {
+        return getBalance();
+    }
+
+    private boolean isSsnValid(String ssn) {
+        //return this.ssn.equals(ssn);    //not null safe
+        return Objects.equals(this.ssn, ssn);   //null safe
+    }
+}
---
+++ src/gr/aueb/cf/ch14/bankapp/SsnNotValidException.java
@@ -0,0 +1,7 @@
+package gr.aueb.cf.ch14.bankapp;
+
+public class SsnNotValidException extends Exception{
+    public SsnNotValidException(String message) {
+        super(message);
+    }
+}
