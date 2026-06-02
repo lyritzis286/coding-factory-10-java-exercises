ed3344396afda8368c98d1b49edda828bffc831c
lyritzis286 <lyritzis286@gmail.com>
2026-06-02 20:58:53 +0300

    Add IRectangle interface and AbstractRectangle and Square classes with area, perimeter, and square check methods

---
+++ src/gr/aueb/cf/ch15/Skeletal_Impl/IRectangle.java
@@ -0,0 +1,7 @@
+package gr.aueb.cf.ch15.Skeletal_Impl;
+
+public interface IRectangle {
+    double getArea();
+    double getPerimeter();
+    boolean isSquare();
+}
---
+++ src/gr/aueb/cf/ch15/Skeletal_Impl/Square.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.ch15.Skeletal_Impl;
+
+public class Square extends AbstractRectangle{
+
+    public Square() {
+
+    }
+
+    public Square(double width, double height) {
+        super(width, height);
+    }
+}
