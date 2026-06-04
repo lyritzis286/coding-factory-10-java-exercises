ad65d7f53e9f47d9fd84ca760d7b2192b170f01c
lyritzis286 <lyritzis286@gmail.com>
2026-06-04 19:14:47 +0300

    Add Main and WorkerThread classes for multithreading example

---
+++ src/gr/aueb/cf/ch16/threads/Main.java
@@ -0,0 +1,21 @@
+package gr.aueb.cf.ch16.threads;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Runnable worker1 = new WorkerThread();
+        Runnable worker2 = new WorkerThread();
+        Runnable worker3 = new WorkerThread();
+        Runnable worker4 = new WorkerThread();
+        Runnable worker5 = new WorkerThread();
+
+        Thread t1 = new Thread(worker1);
+
+        t1.start();
+
+        new Thread(worker2).start();
+        new Thread(worker3).start();
+        new Thread(worker4).start();
+        new Thread(worker5).start();
+    }
+}
