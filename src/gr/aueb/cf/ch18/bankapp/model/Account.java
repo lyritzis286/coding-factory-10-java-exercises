394412270c057799bb53ce697c9a04db81ad94f8
lyritzis286 <lyritzis286@gmail.com>
2026-06-10 20:58:47 +0300

    Add Account model and controller with DTOs for account creation and read-only access

---
+++ src/gr/aueb/cf/ch18/bankapp/model/Account.java
@@ -0,0 +1,52 @@
+package gr.aueb.cf.ch18.bankapp.model;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+/**
+ * Model Entity
+ */
+public class Account {
+    private BigDecimal balance;
+    private String iban;
+
+  public Account() {
+
+  }
+    public Account(String iban ,BigDecimal balance) {
+        this.balance = balance;
+        this.iban = iban;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "IBAN: " + iban + "Balance: " + balance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Account account)) return false;
+        return Objects.equals(getIban(), account.getIban());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getBalance(), getIban());
+    }
+}
