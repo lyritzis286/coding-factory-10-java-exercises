7b56878b489aff53098406c9799c1edcb081d1ee
lyritzis286 <lyritzis286@gmail.com>
2026-06-24 14:26:16 +0300

    Add custom exception classes for account management

---
+++ src/gr/aueb/cf/ch18/bankapp_mysql/bankapp/core/exceptions/NegativeAmountException.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.ch18.bankapp_mysql.bankapp.core.exceptions;
+
+public class NegativeAmountException extends Exception{
+
+
+    public NegativeAmountException(String message) {
+        super(message);
+    }
+}
