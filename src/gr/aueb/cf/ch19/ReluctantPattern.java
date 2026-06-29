683ad4a63266b70566d853007aac8aa7532a6092
lyritzis286 <lyritzis286@gmail.com>
2026-06-29 20:17:24 +0300

    Add GreedyPattern and ReluctantPattern classes for regex matching examples

---
+++ src/gr/aueb/cf/ch19/ReluctantPattern.java
@@ -0,0 +1,20 @@
+package gr.aueb.cf.ch19;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Reluctant Non Greedy
+ */
+public class ReluctantPattern {
+
+    public static void main(String[] args) {
+        String str = "alice;12345;bob;23456;";
+
+        Pattern pattern = Pattern.compile(".*?;");   //Non-Greedy - reluctant match
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
+}
